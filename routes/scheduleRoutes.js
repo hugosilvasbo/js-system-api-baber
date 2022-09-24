6490cf16b7b73c6a4e6f7366e3069c68ae4459b1
Hugo Souza <hugo_sbo04@hotmail.com>
2022-09-24 12:30:13 -0300

    Coisas

--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -6,48 +6,85 @@ const router = require('express').Router()
 const Schedule = require('../models/Schedule')
 
 router.post('/', async (req, res) => {
-    const { person, date } = req.body
+    const {
+        person,
+        date
+    } = req.body
 
-    const schedule = { person, date }
+    const schedule = {
+        person,
+        date
+    }
 
     try {
         await Schedule.create(schedule)
-        res.status(201).json({ message: 'Agendamento realizado!' })
+        res.status(201).json({
+            message: 'Agendamento realizado!'
+        })
     } catch (error) {
-        res.status(500).json({ error: error })
+        res.status(500).json({
+            error: error
+        })
     }
 })
 
 router.get('/', async (req, res) => {
+    console.log('Cai no get!')
     try {
-        let { date } = req.params
-        console.log(req.params)
+        let {
+            startdate,
+            enddate
+        } = req.query
+
+        let query = {
+            startdate,
+            enddate
+        }
+
+        Object.keys(query).map((i) => i == undefined && delete query[i])
 
         const schedule = await Schedule
-            .find(query)
+            .find({
+                date: {
+                    $gte: query.startdate,
+                    $lte: query.enddate
+                }
+            })
             .populate('person')
 
         res.status(200).json(schedule)
     } catch (error) {
-        res.status(500).json({ error: error })
+        res.status(500).json({
+            error: error
+        })
     }
 })
 
 router.delete('/:id', async (req, res) => {
     const id = req.params.id
 
-    const schedule = await Schedule.findOne({ _id: id })
+    const schedule = await Schedule.findOne({
+        _id: id
+    })
 
     if (!schedule) {
-        res.status(422).json({ message: 'Agendamento não encontrado(a)!' })
+        res.status(422).json({
+            message: 'Agendamento não encontrado(a)!'
+        })
         return
     }
 
     try {
-        await Schedule.deleteOne({ _id: id })
-        res.status(200).json({ message: "Agendamento removido!" });
+        await Schedule.deleteOne({
+            _id: id
+        })
+        res.status(200).json({
+            message: "Agendamento removido!"
+        });
     } catch (error) {
-        res.status(500).json({ error: error })
+        res.status(500).json({
+            error: error
+        })
     }
 })
 
